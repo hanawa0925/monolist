0a0aa40c756bc356263c91f853535782ea231e44
hanawa0925 <hanawa0925@gmail.com>
2016-03-13 10:34:40 +0000

    set images and titles

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,6 +8,7 @@ class ItemsController < ApplicationController
                                   :search_index => 'All' , 
                                   :response_group => 'Medium' , 
                                   :country => 'jp')
+
       @amazon_items = response.items
     end
   end
